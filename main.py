9143c1f69dd7ce5d3a212f502d6f68fa0b10e18f
dija27 <abok04@csspo.ca>
2025-11-11 16:55:49 -0500

    file

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+print("welcome to guess the number")
+print ("try to guess the number")
+print("can you guess what it is ")
+number_to_guess = random.randint(1, 100)
+guesses = 0
+
+
+    
+    
+
+
+
+
+
+
+
+
